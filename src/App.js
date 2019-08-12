fb22307abfb3a58394f227e1e15eecc46a93a243
jtmccutcheon <jt.mccutcheon@live.com>
2019-08-12 18:10:20 -0400

    down and quarter botton

--- src/App.js
+++ src/App.js
@@ -7,6 +7,26 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 const [homeScore, setHomeScore] = useState(0)
 const [awayScore, setAwayScore] = useState(0)
+const [quarter, setQuarter] = useState(1)
+const [down, setDown] = useState(1)
+
+const quarterAdd = () => {
+  if (quarter < 4) {
+    setQuarter(quarter + 1)
+  }
+  else{
+    setQuarter(1)
+  }
+}
+
+const downAdd = () => {
+  if(down < 4) {
+    setDown(down + 1)
+  }
+  else{
+    setDown(1)
+  }
+}
 
   return (
     <div className="container">
@@ -20,12 +40,16 @@ const [awayScore, setAwayScore] = useState(0)
             <div className="home__score">{homeScore}</div>
           </div>
           <div className="timer">00:03</div>
+         
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score">{awayScore}</div>
           </div>
         </div>
-        <BottomRow />
+        <BottomRow 
+          quarterDisplay={quarter}
+          downDisplay={down}
+        />
       </section>
       <section className="buttons">
         <div className="homeButtons">
@@ -37,6 +61,13 @@ const [awayScore, setAwayScore] = useState(0)
           <button className="awayButtons__touchdown" onClick={ () => setAwayScore(awayScore + 7)}>Away Touchdown</button>
           <button className="awayButtons__fieldGoal" onClick={ () => setAwayScore(awayScore + 3)}>Away Field Goal</button>
         </div>
+        <div className="quarterButton">
+          <button className="quarterButton__change" onClick={ () => quarterAdd()}>Quarter</button>
+        </div>
+        <div className="downButton">
+          <button className="downButton__change" onClick={ () => downAdd()}>Down</button>
+        </div>
+
       </section>
     </div>
   );
